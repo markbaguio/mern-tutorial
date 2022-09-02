e2a78de7205e33fa096f758d3e68c2dd5b103f10
Mark Baguio <godwinbaguio@gmail.com>
2022-09-02 14:15:00 +0800

    Frontend in progress Added react and redux toolkit Created a header for navigation Added Routing capability using react-router-dom

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header /> {/**this is the Header.jsx component. */}
+          <Routes>
+            {/** these routes refers to the url 
+             * ex. localhost:3000/login will
+            render the login page */}
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
