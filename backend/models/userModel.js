fd9b467b7d4b3aab7655e71f99bdfacb5c934f0a
markbaguio <godwinbaguio@gmail.com>
2022-12-04 13:52:25 +0800

    model update

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      reuired: [true, "Please add a name."],
+      required: [true, "Please add a name."],
     },
     email: {
       type: String,
